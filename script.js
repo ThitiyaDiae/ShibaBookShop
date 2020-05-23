5e1df8a791a6eec6f27e0282336565f2fa6408bb
ThitiyaDiae <52684898+ThitiyaDiae@users.noreply.github.com>
2020-05-23 16:44:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,171 @@
+// debug URL ที่เรียก
+console.log("https://oas.kotchasan.com/api.php/categories");
+// ใช้ Ajax โหลดข้อมูลหมวดหมู่มาทำเป็นเมนู
+new GAjax({ method: "GET" }).send(
+  "https://oas.kotchasan.com/api.php/categories",
+  null,
+  function(xhr) {
+    // debug ผลตอบกลับจาก Server
+    console.log(xhr.responseText); // แปลงข้อมูลตอบกลับเป็น JSON Object
+    var ds = xhr.responseText.toJSON(), // menu element #categorymenu
+      menu = $G("categorymenu");
+    if (ds) {
+      // วนลูปสร้างรายการเมนูหมวดหมู่
+      for (var i in ds) {
+        var li = menu.create("li");
+        li.innerHTML =
+          '<a href="index.html?category_id=' +
+          ds[i].category_id +
+          '" onclick="return getProducts(\'' +
+          ds[i].category_id +
+          "/1')\"><span>" +
+          ds[i].topic +
+          "</span></a>";
+        li.id = ds[i].category_id + "/1";
+      } // เรียกใช้งาน Javascript Drop Down Menu เพื่อให้รองรับ Responsive (ไม่ใช้ก็ได้)
+      new GDDMenu("topmenu"); // อ่านค่าจาก Addressbar แล้วมาแยกเอา id หรือ category_id และ page ที่เป็น parameter ออก
+      var urls = /((category_)?id)=([0-9]+)(&page=([0-9]+))?/.exec(
+        window.location.toString()
+      );
+      if (urls && urls[1] == "category_id") {
+        // ถ้ามี parameter ครบ ไปโหลดหน้าเว็บที่ต้องการมาแสดง
+        getProducts(urls[3] + "/" + (urls[5] ? urls[5] : 1));
+      } else if (urls && urls[1] == "id") {
+        // แสดงรายละเอียดสินค้าที่กำหนด
+        getProduct(urls[3]);
+      } else {
+        // ถ้าไม่มี ตรวจสอบ category_id จากเมนูรายการแรก
+        urls = /category_id=([0-9]+)/.exec(menu.firstChild.firstChild.href);
+        // แสดงหน้าแรก
+        getProducts(urls[1] + "/1");
+      }
+    }
+  }
+);
+function getProduct(id) {
+  // debug URL ที่เรียก
+  console.log("https://oas.kotchasan.com/api.php/product/" + id);
+  // ใช้ Ajax โหลดข้อมูลตามที่เลือก
+  new GAjax({ method: "GET" }).send(
+    "https://oas.kotchasan.com/api.php/product/" + id,
+    null,
+    function(xhr) {
+      // debug ผลตอบกลับจาก Server
+      console.log(xhr.responseText);
+      // แปลงข้อมูลตอบกลับเป็น JSON Object
+      var ds = xhr.responseText.toJSON(),
+        detail = "";
+      // #content ส่วนแสดงผลเนื้อหา
+      content = $G("content");
+      if (ds) {
+        document.title = ds.topic;
+        detail +=
+          '<h2><a href="index.html?id=' + ds.id + '">' + ds.topic + "</a></h2>";
+        detail += '<div class="ggrid"><div class="float-left block6">';
+        detail += '<img src="' + ds.image + '" alt="' + ds.topic + '">';
+        detail += '</div><div class="float-left block6">';
+        detail += "<h3>" + ds.topic + "</h3>";
+        detail += "<h4>รหัสสินค้า : " + ds.product_no + "</h4>";
+        detail += "<p>" + ds.description + "</p>";
+        detail += "<p>ราคา <em>" + ds.price + "</em> บาท</p>";
+        detail += "</div></div>";
+        // แสดงผลข้อมูลลงใน #content
+        content.innerHTML = detail;
+        // เลื่อนขึ้นไปด้านบน
+        window.scrollTo(0, content.getTop() - 10);
+      }
+    }
+  );
+  return false;
+}
+function getProducts(id) {
+  // debug URL ที่เรียก
+  console.log("https://oas.kotchasan.com/api.php/products/" + id);
+  // ใช้ Ajax โหลดข้อมูลตามที่เลือก
+  new GAjax({ method: "GET" }).send(
+    "https://oas.kotchasan.com/api.php/products/" + id,
+    "limit=20",
+    function(xhr) {
+      // debug ผลตอบกลับจาก Server
+      console.log(xhr.responseText);
+      // แปลงข้อมูลตอบกลับเป็น JSON Object
+      var ds = xhr.responseText.toJSON(),
+        detail = "",
+        item,
+        col = 4,
+        n = 0;
+      // #content ส่วนแสดงผลเนื้อหา
+      content = $G("content");
+      if (ds) {
+        document.title = ds.category;
+        detail += "<h2>" + ds.category + "</h2>";
+        // วนลูปรายการ ds.items เพื่อแสดงรายการสินค้า โดยใช้ griid ในการแสดงผล
+        detail += '<div class="document-list thumbview"><div class="row">';
+        for (var i in ds.items) {
+          if (n > 0 && n % col == 0) {
+            detail += '</div><div class="row">';
+          }
+          item = ds.items[i];
+          detail += '<article class="col' + col + '">';
+          detail +=
+            '<a class="figure" href="index.html?id=' +
+            item.id +
+            '" onclick="return getProduct(' +
+            item.id +
+            ')">';
+          detail +=
+            '<img class=nozoom src="' +
+            item.image +
+            '" alt="' +
+            item.topic +
+            '">';
+          detail += "</a><div>";
+          detail +=
+            '<h6><a href="index.html?id=' +
+            item.id +
+            '" onclick="return getProduct(' +
+            item.id +
+            ')">' +
+            item.topic +
+            "</a></h6>";
+          detail += '<p class="price">' + item.price + " THB</p>";
+          detail += "</div></article>";
+          n++;
+        }
+        detail += "</div></div>";
+        // ลิงค์รายการแบ่งหน้า (ถ้ามี)
+        if (ds.totalpage > 0) {
+          detail += '<footer class="splitpage">';
+          for (i = 1; i <= ds.totalpage; i++) {
+            if (i == ds.page) {
+              detail += "<strong>" + i + "</strong>";
+            } else {
+              detail +=
+                '<a href="index.html?category_id=' +
+                ds.category_id +
+                "&amp;page=" +
+                i +
+                '" id="' +
+                ds.category_id +
+                "/" +
+                i +
+                '" onclick="return getProducts(\'' +
+                ds.category_id +
+                "/" +
+                i +
+                "')\">" +
+                i +
+                "</a>";
+            }
+          }
+          detail += "</footer>";
+        }
+        // แสดงผลข้อมูลลงใน #content
+        content.innerHTML = detail;
+        // เลื่อนขึ้นไปด้านบน
+        window.scrollTo(0, content.getTop() - 10);
+      }
+    }
+  );
+  return false;
+}
---
+++ store.js
@@ -0,0 +1,210 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
+
+    var addToCartButtons = document.getElementsByClassName('shop-item-button')
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("shop-header");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+function purchaseClicked() {
+    var cash = prompt('cash')
+    if(cash==total()){
+        alert('Thank you for your purchase')
+    }else{
+        var change = cash - total()
+        alert('change: '+change+'B'+'\nThank you for your purchase')
+    }
+    
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    updateCartTotal()
+}
+
+function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+}
+
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
+
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    addItemToCart(title, price, imageSrc)
+    updateCartTotal()
+}
+
+function addItemToCart(title, price, imageSrc) {
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    for (var i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText == title) {
+            alert('This item is already added to the cart')
+            return
+        }
+    }
+    var cartRowContents = `
+        <div class="cart-item cart-column">
+            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">REMOVE</button>
+        </div>`
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+}
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    var discount = 0
+
+    for (var i = 0; i < cartRows.length; i++){
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('B', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+
+        if(quantity==1){
+            discount = discount + 0
+            break;
+        }else if(quantity==2){
+            discount = discount + Math.round((price * 0.1)*quantity) 
+            break;
+        }else if(quantity==3){
+            discount = discount + Math.round((price * 0.11)*quantity) 
+            break;
+        }else if(quantity==4){
+            discount = discount + Math.round((price * 0.12)*quantity) 
+            break;
+        }else if(quantity==5){
+            discount = discount + Math.round((price * 0.13)*quantity) 
+            break;
+        }else if(quantity==6){
+            discount = discount + Math.round((price * 0.14)*quantity) 
+            break;
+        }else{
+            discount = discount + Math.round((price * 0.15)*quantity) 
+            break;
+        }
+    }
+
+    discount = (Math.round(discount * 100) / 100)
+    document.getElementsByClassName('cart-discount-price')[0].innerText = discount+' '+'B' 
+
+    total = (Math.round(total * 100) / 100)-discount
+    document.getElementsByClassName('cart-total-price')[0].innerText = total+' ' + 'B'
+}
+
+function total() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    var discount = 0
+
+    for (var i = 0; i < cartRows.length; i++){
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('B', ''))
+        var quantity = quantityElement.value
+
+        if(quantity==1){
+            discount = discount + 0
+            break;
+        }else if(quantity==2){
+            discount = discount + Math.round((price * 0.1)*quantity) 
+            break;
+        }else if(quantity==3){
+            discount = discount + Math.round((price * 0.11)*quantity) 
+            break;
+        }else if(quantity==4){
+            discount = discount + Math.round((price * 0.12)*quantity) 
+            break;
+        }else if(quantity==5){
+            discount = discount + Math.round((price * 0.13)*quantity) 
+            break;
+        }else if(quantity==6){
+            discount = discount + Math.round((price * 0.14)*quantity) 
+            break;
+        }else{
+            discount = discount + Math.round((price * 0.15)*quantity) 
+            break;
+        }
+    }
+
+    discount = (Math.round(discount*100)/100)
+
+    for (var i = 0; i < cartRows.length; i++){
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('B', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+
+    total = (Math.round(total * 100) / 100)-discount
+
+    return total;
+}
